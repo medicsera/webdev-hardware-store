224b43cbcedaa7b6aac3ef9ef40329f7204a9ae9
medicsera <anton.mirko.0550@gmail.com>
2026-03-05 16:20:54 +0700

    init project

---
+++ src/main/kotlin/com/example/webdev_hardware_store/WebdevHardwareStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.webdev_hardware_store
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebdevHardwareStoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebdevHardwareStoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/webdev_hardware_store/WebdevHardwareStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.webdev_hardware_store
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebdevHardwareStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
