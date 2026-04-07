7e5ab5c3b0da688207760377b75355a40c2d55a1
medicsera <anton.mirko.0550@gmail.com>
2026-04-07 16:45:11 +0700

    build (vite.config.ts) : add scss, outDir in vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css:{
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/variables.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: '../backend/src/main/resources/static',
+    emptyOutDir: true
+  }
 })
