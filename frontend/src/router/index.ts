575ac5dee477855d9d1f1ea986dbde44a2e826e1
medicsera <anton.mirko.0550@gmail.com>
2026-04-11 21:30:16 +0700

    fix : router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,32 +1,30 @@
-import {createRouter, createWebHistory } from "vue-router";
-import type {RouteRecordRaw} from "vue-router";
-
-import HomeView from "@/views/HomeView.vue";
-import CatalogView from '@/views/CatalogView.vue';
-import ProductView from '@/views/ProductView.vue';
-import CartView from '@/views/CartView.vue';
+import { createRouter, createWebHistory } from "vue-router"
+import type { RouteRecordRaw } from "vue-router"
 
 const routes: RouteRecordRaw[] = [
     {
         path: "/",
         name: "home",
-        component: () => HomeView,
-        meta: {title: "Главная"}
+        component: () => import('@/views/HomeView.vue'),
+        meta: { title: "Главная" }
     },
     {
         path: '/catalog',
         name: 'catalog',
-        component: () => CatalogView
+        component: () => import('@/views/CatalogView.vue'),
+        meta: { title: "Каталог" }
     },
     {
         path: '/product/:id',
         name: 'product',
-        component: () => ProductView
+        component: () => import('@/views/ProductView.vue'),
+        meta: { title: "Товар" }
     },
     {
         path: '/cart',
         name: 'cart',
-        component: () => CartView
+        component: () => import('@/views/CartView.vue'),
+        meta: { title: "Корзина" }
     }
 ]
 
@@ -34,12 +32,12 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
     scrollBehavior(to, from, savedPosition) {
-        return savedPosition || {top: 0}
+        return savedPosition || { top: 0 }
     }
 })
 
 router.beforeEach((to) => {
-    document.title = `${to.meta.title || 'Factura Shop'} | Factura Shop `
+    document.title = `${to.meta.title || 'Factura Shop'} | Factura Shop`
 })
 
 export default router
